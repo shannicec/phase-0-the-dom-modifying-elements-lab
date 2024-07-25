a9c227e270dca32ce96b925f7f811a69286587fe
shannicec <shanice.cheptoo@student.moringaschool.com>
2024-07-25 19:29:09 +0300

    dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// Remove the <main> element if it exists
+const mainElement = document.getElementById("main");
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your name
+
+document.body.append(newHeader);
